e9e4778e912c8e269efabecda44db2b2ba51d4cf
Scot Rumery <scot@rumspeed.com>
2015-12-23 09:27:12 -0500

    FIX: remove SERVER_PORT so URL matches "Tab URL"

--- readme.txt
+++ readme.txt
@@ -69,6 +69,7 @@ No. We want to keep it simple so we are only supporting one tab. More than one t
 * Tested up to: 4.4
 * FIX: Undefined index: text_shadow - on line 405
 * FIX: PHP notices: Undefined index SERVER_PORT and SERVER_NAME when using WP_CLI
+* FIX: Tab URL with HTTPS was still showing on target page when it shouldn't
 
 = 1.1.2 =
 * Tested up to: 4.1
--- simple_side_tab.php
+++ simple_side_tab.php
@@ -108,8 +108,8 @@ function rum_get_full_url()
 		$s 			= empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
 		$sp 		= strtolower($_SERVER["SERVER_PROTOCOL"]);
 		$protocol 	= substr($sp, 0, strpos($sp, "/")) . $s;
-		$port 		= ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]);
-		return $protocol . "://" . $_SERVER['SERVER_NAME'] . $port . $_SERVER['REQUEST_URI'];
+
+		return $protocol . "://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 	}
 }
 
