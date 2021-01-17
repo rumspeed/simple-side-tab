ee5023bf4d54bf07f1c241b336785e9b16b701e3
Scot Rumery <scot@rumspeed.com>
2021-01-17 16:24:22 -0500

    update license

--- readme.txt
+++ readme.txt
@@ -5,8 +5,8 @@ Tags: tab, navigation, admin, call to action, page link, mobile tab, browser tab
 Requires at least: 4.6
 Tested up to: 5.0
 Stable tag: 1.2.1
-License: GPLv2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
 Display a side tab that you can easily link to any page. Customize the tab text, font and colors. It's that simple. That's Simple Side Tab.
 
--- simple_side_tab.php
+++ simple_side_tab.php
@@ -6,24 +6,8 @@ Description: Display a side tab that you can easily link to any page. Customize 
 Version: 1.2.1
 Author: Scot Rumery
 Author URI: http://rumspeed.com/scot-rumery/
-License: GPLv2
-*/
-
-/*  Copyright 2016  Scot Rumery (email : scot@rumspeed.com)
-
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
 
 
