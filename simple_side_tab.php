f2296d5bb243c9d336a9eb6cd6bff02414aafbe2
Scot Rumery <scot@rumspeed.com>
2021-01-27 17:12:46 -0500

    Including Software License comment block

--- simple_side_tab.php
+++ simple_side_tab.php
@@ -15,6 +15,24 @@
 
 
 
+/*
+Simple Side Tab is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+Simple Side Tab is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Simple Side Tab. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+*/
+
+
+
+
 // Hook will fire upon activation - we are using it to set default option values
 register_activation_hook( __FILE__, 'rum_sst_activate_plugin' );
 
