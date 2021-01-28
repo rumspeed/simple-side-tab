35dbed3d931659a762758972b52072c12e79d90e
Scot Rumery <scot@rumspeed.com>
2021-01-28 12:13:31 -0500

    initial pass at using WordPress-Plugin-Boilerplate setup

--- simple_side_tab.php
+++ simple_side_tab.php
@@ -33,6 +33,92 @@ along with Simple Side Tab. If not, see https://www.gnu.org/licenses/gpl-3.0.htm
 
 
 
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+
+
+
+/**
+ * Currently plugin version.
+ */
+define( 'SIMPLE_SIDE_TAB_VERSION', '1.2.3' );
+
+
+
+
+// /**
+//  * The code that runs during plugin activation.
+//  * This action is documented in includes/class-plugin-name-activator.php
+//  */
+// function activate_plugin_name() {
+// 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
+// 	Plugin_Name_Activator::activate();
+// }
+
+
+
+
+// /**
+//  * The code that runs during plugin deactivation.
+//  * This action is documented in includes/class-plugin-name-deactivator.php
+//  */
+// function deactivate_plugin_name() {
+// 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
+// 	Plugin_Name_Deactivator::deactivate();
+// }
+
+
+
+
+// register_activation_hook( __FILE__, 'activate_plugin_name' );
+// register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
+
+
+
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-simple-side-tab.php';
+
+
+
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_simple_side_tab() {
+
+	$plugin = new Simple_Side_Tab();
+	$plugin->run();
+
+}
+
+
+
+
+run_simple_side_tab();
+
+
+
+
+
+
+
+
+
+
+
 // Hook will fire upon activation - we are using it to set default option values
 register_activation_hook( __FILE__, 'rum_sst_activate_plugin' );
 
