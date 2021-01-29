ae3e2610560c4cc8882b841694444be86de06b0c
Scot Rumery <scot@rumspeed.com>
2021-01-29 08:01:42 -0500

    move public functions to class methods

--- includes/class-simple-side-tab.php
+++ includes/class-simple-side-tab.php
@@ -184,10 +184,10 @@ class Simple_Side_Tab {
 
 		$plugin_public = new Simple_Side_Tab_Public( $this->get_plugin_name(), $this->get_version() );
 
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+        $this->loader->add_action( 'wp', $plugin_public, 'display_tab' );
+        $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
-
-	}
+    }
 
 
 
