4558a187de4de686c78fba400f6403a54f6f1d62
Scot Rumery <scot@rumspeed.com>
2021-02-20 13:22:23 -0500

    FIX default checkbox state by setting initial values to NULL

--- includes/class-simple-side-tab-options.php
+++ includes/class-simple-side-tab-options.php
@@ -107,17 +107,15 @@ class Simple_Side_Tab_Options {
         // return default option values; used in class Simple_Side_Tab_Activator
         return array(
             'text_for_tab'     => '',
-            'font_family'      => 'Tahoma, sans-serif',
-            'font_weight_bold' => '1',
-// TODO: for some reason this is being set to TRUE on the front end when plugin is activated
-            'text_shadow'      => '0',
             'tab_url'          => '',
+            'font_family'      => 'Tahoma, sans-serif',
+            'font_weight_bold' => null,
+            'text_shadow'      => null,
+            'target_blank'     => null,
             'pixels_from_top'  => '350',
             'text_color'       => '#ffffff',
             'tab_color'        => '#a0244e',
             'hover_color'      => '#a4a4a4',
-// TODO: for some reason this is being set to TRUE on the front end when plugin is activated
-            'target_blank'     => '0',
             'left_right'	   => 'left'
             );
     }
